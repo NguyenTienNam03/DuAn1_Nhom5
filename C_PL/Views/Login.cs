2bca8ad9c8f4893ed08d43f6ee88fbcd21881d56
MSI\ducnguyenvann <ducnvph24570@fpt.edu.vn>
2022-11-25 15:24:28 +0700

    thay doi

--- C_PL/Views/Login.cs
+++ C_PL/Views/Login.cs
@@ -35,7 +35,7 @@ namespace C_PL.Views
                 MessageBox.Show(" Đăng Nhập Thành Công");
                 GiaoDien gd = new GiaoDien();
                 gd.ShowDialog();
-
+// jjejej
 
             }
         }
