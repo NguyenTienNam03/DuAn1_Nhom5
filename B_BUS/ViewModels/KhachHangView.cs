ce18b1f82eb228efd81cbb60812a824cbb554b85
WINDOWS-10\Administrator <namntph23972@fpt.edu.vn>
2022-11-28 15:24:34 +0700

    capnhat

--- B_BUS/ViewModels/KhachHangView.cs
+++ B_BUS/ViewModels/KhachHangView.cs
@@ -14,8 +14,6 @@ namespace B_BUS.ViewModels
         public string MaKH { get; set; }
         public string DiaChi { get; set; }
         public string SDT { get; set; }
-        public string MatKhau { get; set; }
-        public string Email { get; set; }
-        public string TrangThai { get; set; }
+        
     }
 }
